acebec81466bb91517b683086a48aab89cd45f77
Andrea F. Daniele <afdaniele@ttic.edu>
2018-08-17 13:11:59 -0500

    added source ROS

--- assets/launch_websocket_bridge.sh
+++ assets/launch_websocket_bridge.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# source ROS and code
+source /opt/ros/$ROS_DISTRO/setup.bash
+
+# launch websocket bridge
 roslaunch --wait /root/rosbridge_websocket.launch port:=$WEBSOCKET_BRIDGE_PORT
