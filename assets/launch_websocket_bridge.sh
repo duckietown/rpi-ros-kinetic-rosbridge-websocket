0cb63e76ea8eb804c66e0929aced77314302396d
Andrea Daniele <andreadaniele@AndreadielesMBP.lan>
2019-03-17 01:27:22 -0700

    sourced duckietown code

--- assets/launch_websocket_bridge.sh
+++ assets/launch_websocket_bridge.sh
@@ -3,5 +3,8 @@
 # source ROS and code
 source /opt/ros/$ROS_DISTRO/setup.bash
 
+# source Duckietown code
+source /home/software/docker/env.sh
+
 # launch websocket bridge
 roslaunch --wait /root/rosbridge_websocket.launch port:=$WEBSOCKET_BRIDGE_PORT
