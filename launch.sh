cd61f8e65dee67f28ffac0d2ff9251f026ec2782
Andrea F. Daniele <afdaniele@ttic.edu>
2020-06-30 14:22:16 -0500

    moved rosbridge node under veh namespace

--- launch.sh
+++ launch.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-set -e
+source /environment.sh
+
+# initialize launch file
+dt-launchfile-init
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-roslaunch \
-  --wait \
-  ${DIR}/assets/rosbridge_websocket.launch \
-    port:=${WEBSOCKET_BRIDGE_PORT}
+# launching app
+dt-exec \
+  roslaunch \
+    --wait \
+    ${DIR}/assets/rosbridge_websocket.launch \
+      port:=${WEBSOCKET_BRIDGE_PORT} \
+      veh:="$VEHICLE_NAME"
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
+
+# wait for app to end
+dt-launchfile-join
