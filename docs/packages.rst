e931f209a7edd1a61559a6b3039c495db2a01333
Andrea F. Daniele <afdaniele@ttic.edu>
2020-07-09 14:53:53 -0500

    adapted to template v2

---
+++ docs/packages.rst
@@ -0,0 +1,8 @@
+Duckietown Module: dt-rosbridge-websocket
+=========================================
+
+.. toctree::
+   :glob:
+   :maxdepth: 4
+
+   packages/*
--- launchers/default.sh
+++ launchers/default.sh
@@ -8,15 +8,14 @@ dt-launchfile-init
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # launching app
-dt-exec \
-  roslaunch \
-    --wait \
-    ${DIR}/assets/rosbridge_websocket.launch \
-      port:=${WEBSOCKET_BRIDGE_PORT} \
-      veh:="$VEHICLE_NAME"
+dt-exec roslaunch \
+  --wait \
+  ${DT_REPO_PATH}/assets/rosbridge_websocket.launch \
+    port:=${WEBSOCKET_BRIDGE_PORT} \
+    veh:="$VEHICLE_NAME"
+
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
