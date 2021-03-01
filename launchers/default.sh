2a590a7a548beba175deb6625375d2c5d5db648b
Andrea F. Daniele <afdaniele@ttic.edu>
2021-03-01 10:57:29 -0600

    added call to `dt-set-module-healthy` in launcher

--- launchers/default.sh
+++ launchers/default.sh
@@ -9,6 +9,12 @@ dt-launchfile-init
 # ----------------------------------------------------------------------------
 
 
+# NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
+# NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
+
+# set module's health
+dt-set-module-healthy
+
 # launching app
 dt-exec roslaunch \
   --wait \
